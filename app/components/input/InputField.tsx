8c427f4b3902e98e6b75cca782f79df94188a953
usman359 <m.usman095@gmail.com>
2024-12-18 11:38:38 +0500

    remove header.tsx

--- app/components/input/InputField.tsx
+++ app/components/input/InputField.tsx
@@ -1,10 +1,18 @@
+interface InputFieldProps {
+  label: string;
+  type: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+}
+
 export default function InputField({
   label,
   type,
   value,
   onChange,
   placeholder,
-}) {
+}: InputFieldProps) {
   return (
     <div className="mb-4">
       <label className="block text-gray-700 font-semibold mb-2">{label}</label>
